080553523e99690922431d7d45e6fb8a2bff62f3
Swarup Mandal <swarupmandal630@gmail.com>
2026-06-10 22:18:15 +0530

    render issue solutuion test 2

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 from accounts.views import landing_page
-from django.http import HttpResponse
 
 urlpatterns = [
     path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.ico', permanent=True)),
-    path("health/", lambda request: HttpResponse("OK")),
+    path("health/", health),
     path('django-admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('cooks/',    include('cooks.customer_urls', namespace='customer')),
